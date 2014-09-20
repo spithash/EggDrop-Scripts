8d8a21955921218d6fc1c2cfde3fb875d0105bd8
Patrick Hudson <Patrick@Patricks-Mac-Pro.local>
2014-09-20 12:53:55 -0500

    Added license header, and removed random debug code

--- register.tcl
+++ register.tcl
@@ -1,7 +1,26 @@
-# The trigger
+#Simple EggDrop script that allows users to Register to your bot. It updates hostmasks at login to make sure that no one gets lost in the userfile
+#Copyright (C) 2014 Patrick Hudson (https://patrick.hudson.bz)
+#
+#
+#Register for Eggdrop is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 2 of the License, or
+#(at your option) any later version.
+#
+#Register for Eggdrop is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with Register for Eggdrop.  If not, see <http://www.gnu.org/licenses/>.
+
+#God speed my friend
+
+# The trigger - set this to anything you'd like
 set pubtrig "!"
 
-# ---- EDIT END ----
+# ---- STOP EDITING HERE. This code was created, and only myself and god knew how it worked. Now only god knows ---- #
 proc getTrigger {} {
   global pubtrig
   return $pubtrig
@@ -13,14 +32,9 @@ bind join - "#* *!*@*" join:add
 
 proc join:add {nick host handle chan} {
     foreach user [userlist] {
-        #putlog "$nick"
         set handle [string range "$nick" 0 8]
-        #putlog "$handle"
-        #putlog "$nick"
         if {$user == $handle} {
-          #putlog "$handle"
           setuser $handle HOSTS [maskhost $host]
-          #adduser $nick [maskhost $host]
           putlog "Register: <${nick}/${chan}> succesfully updated host to $host"
         }
     }
